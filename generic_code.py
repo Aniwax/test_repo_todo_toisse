97abaaf1cc3dd09571fed61ca78995b4c1e0ca00
Shirzart Enwer <aniwax@iwf.mavt.ethz.ch>
2021-06-17 14:19:58 +0200

    feat: some progress on the test branch

--- generic_code.py
+++ generic_code.py
@@ -3,3 +3,8 @@ import random
 # TODO: add more code here and maybe refactor a bit 
 number_1 = random.randint(1, 1000)
 number_2 = random.randint(2, 1000)
+
+# TODO: think about some cool functionalities
+
+print("This is the so called more functionalities.")
+print("Goodbye, world!")
